6cc813ca2403a3149e008635e206414298151d97
fr0stsp1re <info@atomiccomputerservice.com>
2018-02-21 10:24:13 -0800

    Changed UI to add second team. Changed java code to add second team.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -13,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
 
+    /**
+     * Set up a variable to keep score for team B
+     */
+
+    int scoreTeamB = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,34 +29,66 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Displays the given score for Team A.
      */
-
     public void displayForTeamA(int score) {
-
         TextView scoreView = findViewById(R.id.team_a_score);
-
         scoreView.setText(String.valueOf(score));
-
-
     }
 
+    /**
+     * Increase score by 3 for team A
+     */
     public void threePointsForTeamA(View v) {
-
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
-
     }
 
+    /**
+     * Increase score by 2 for team A
+     */
     public void twoPointsForTeamA(View v) {
-
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
 
-    public void freeThrow(View v) {
-
+    /**
+     * Increase score by 1 for team A
+     */
+    public void freeThrowForTeamA(View v) {
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
     }
 
+
+    /**
+     * Displays the given score for Team B
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Increase score by 3 for team B
+     */
+    public void threePointsForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase score by 2 for team B
+     */
+    public void twoPointsForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase score by 1 for team B
+     */
+    public void freeThrowForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
 }
 
